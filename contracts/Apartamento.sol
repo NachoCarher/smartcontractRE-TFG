c002e0fb9c67fbca7c4e9db3119eb17601c3d62c
NachoCarvajal <nachoch64@correo.ugr.es>
2022-08-21 18:00:05 +0200

    Test 3: Se debe poder pagar la renta al contrato

--- contracts/Apartamento.sol
+++ contracts/Apartamento.sol
@@ -7,9 +7,16 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Apartamento is ERC20 {
 
+    uint public balance;
+
     constructor() ERC20("ContratoApartamento", "APRTM") {
         // Asignar 100 tokens (100% de las acciones del piso) al propietario del contrato 
         super._mint(_msgSender(), 100);
         console.log("Desplegando contrato Apartamento");
+    }
+
+    receive() external payable {
+        console.log("Recibiendo tokens");
+        balance += msg.value;
     }
 }
--- test/Apartamento.ts
+++ test/Apartamento.ts
@@ -5,6 +5,7 @@ let propietario, Pedro, Juan, Marta;
 
 describe("Apartamento", function () {
 
+  // Caso de test 1
   it("El creador del contrato debe tener 100 acciones del apartamento",async () => {
     const Apartamento = await ethers.getContractFactory("Apartamento");
     const apartamento = await Apartamento.deploy();
@@ -19,6 +20,7 @@ describe("Apartamento", function () {
     expect(balancePropietario).to.equal(100);
   })
 
+  // Caso de test 2
   it("Se debe poder transferir una cantidad de acciones a otro individuo", async () => {
     const Apartamento = await ethers.getContractFactory("Apartamento");
     const apartamento = await Apartamento.deploy();
@@ -27,10 +29,30 @@ describe("Apartamento", function () {
 
     await apartamento.deployed();
     await apartamento.transfer(Marta.address, 40);
+
     expect(await apartamento.balanceOf(Marta.address)).to.equal(40);
     expect(await apartamento.balanceOf(propietario.address)).to.equal(60);
   })
 
-  
+  // Caso de test 3
+  it("Se debe poder pagar el alquiler y despositarlo en ether en el contrato del apartamento", async () => {
+    const Apartamento = await ethers.getContractFactory("Apartamento");
+    const apartamento = await Apartamento.deploy();
+
+    [propietario, Marta, Pedro] = await ethers.getSigners();
+
+    await apartamento.deployed();
+
+    // Un inquilino paga el alquiler y se desposita en ether en la direccion del contrato
+    await Pedro.sendTransaction({
+      to: apartamento.address,
+      value: ethers.utils.parseEther("5")
+    })
+
+    // Se consulta el saldo del smart contract
+    expect(await apartamento.balance()).to.equal(ethers.utils.parseEther("5"));
+  })
+
+
 
 });
